6995aac3c4b4e00d53ce86d96a9d5a38297f258d
vicvinc <vicvinc@192.168.3.4>
2019-04-11 15:45:55 +0800

    up: deploy

--- app.js
+++ app.js
@@ -6,19 +6,14 @@ const app = express();
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, "./build")));
 
-// An api endpoint that returns a short list of items
-app.get("/api/getList", (req, res) => {
-  var list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log("Sent list of items");
-});
-
 // Handles any requests that don't match the ones above
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "./build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+const port = process.env.PORT || 8080;
 
-console.log("App is listening on port " + port);
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+  console.log("Press Ctrl+C to quit.");
+});
